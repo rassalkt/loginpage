e361596af395208ddc91619a795e2571dc4a4acf
unknown <rasal.meachu@gmail.com>
2021-12-01 12:28:35 +0530

    version 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn loginpro.wsgi --log-file -
--- loginpro/settings.py
+++ loginpro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(1&7flfxr4tl*6yoz8&$n%#rl22ou1(wof-m#bm)1i#-iiyzoo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
